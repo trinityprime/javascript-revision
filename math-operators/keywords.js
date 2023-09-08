f91081cc624056036433980072e47e691399a11c
trinityprime <80157949+trinityprime@users.noreply.github.com>
2023-09-08 20:14:44 +0800

    first commit

---
+++ math-operators/keywords.js
@@ -0,0 +1,35 @@
+// Addition
+
+add = 20 + 10;
+console.log(add);
+
+// Subtraction
+
+minus = 20 - 10;
+console.log(minus);
+
+// Multiplication
+
+times = 20 * 10;
+console.log(times);
+
+// Division
+
+divide = 20 / 10;
+console.log(divide);
+
+// Math.round() rounds to 1s.f
+
+divide = 40 / 9;
+divide = Math.round(divide);
+console.log(divide);
+
+// Modulus (Give remainder)
+
+mod = 9 % 2;
+console.log(mod);
+
+// Exponential
+
+expo = 9 ** 2
+console.log(expo)
---
+++ variables/keywords.js
@@ -0,0 +1,18 @@
+// let 
+
+let myName = "prime"
+console.log(myName)
+
+myName = "khora"
+console.log(myName)
+
+console.log(typeof myName)
+
+// const
+
+const myObj = "box"
+console.log(myObj)
+myObj = "car"
+console.log(myObj) // cannot change var with const keyword. 
+
+
